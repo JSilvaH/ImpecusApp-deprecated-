958f2d164ee2ee13248fbd988dc63048d73835da
Jsilva <jjesussh31@gmail.com>
2019-11-23 11:41:58 -0600

    Login Sketch Language Support

---
+++ app/src/main/java/com/itags/mx/impecusapp/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.itags.mx.impecusapp.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/com/itags/mx/impecusapp/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.itags.mx.impecusapp.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
