210d398cd928d07a2044ee078112ae74020a9ac8
Jsilva <jjesussh31@gmail.com>
2019-11-25 15:02:50 -0600

    New sketch of first screen

---
+++ app/src/main/java/com/itags/mx/impecusapp/LoginAndSingupActiviy.kt
@@ -0,0 +1,12 @@
+package com.itags.mx.impecusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginAndSingupActiviy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_and_singup)
+    }
+}
