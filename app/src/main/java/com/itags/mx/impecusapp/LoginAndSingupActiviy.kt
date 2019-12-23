6c4be2e7baa969ba0246bbd33c3527cda1cde6bd
Jsilva <jjesussh31@gmail.com>
2019-12-23 11:49:47 -0600

    Implementing anko commons and sketch of singup activity

--- app/src/main/java/com/itags/mx/impecusapp/LoginAndSingupActiviy.kt
+++ app/src/main/java/com/itags/mx/impecusapp/LoginAndSingupActiviy.kt
@@ -1,12 +1,29 @@
 package com.itags.mx.impecusapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.itags.mx.impecusapp.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_login_and_singup.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivity
+import android.widget.Button as Button
 
 class LoginAndSingupActiviy : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_and_singup)
+
+        btnLoginStart.setOnClickListener {
+            startActivity(intentFor<LoginActivity>())
+        }
+
+        btnSingUp.setOnClickListener {
+            startActivity(intentFor<SingupActivity>())
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/itags/mx/impecusapp/SingupActivity.kt
@@ -0,0 +1,12 @@
+package com.itags.mx.impecusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singup_activity)
+    }
+}
